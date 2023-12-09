6b67f42e28ca416873f180c073d6c065a9fdabc8
Ruthenic <mdrakea3@tutanota.com>
2023-12-08 20:55:28 -0500

    add custom reply mention

---
+++ src/customReplyMention/index.ts
@@ -0,0 +1,45 @@
+import { ExtensionWebpackModule, Patch } from "@moonlight-mod/types";
+
+let defaultPing = moonlight.getConfigOption<boolean>(
+  "customReplyMention",
+  "shouldPing",
+) ?? true;
+let shouldInvertOnShift = moonlight.getConfigOption<boolean>(
+  "customReplyMention",
+  "shouldInvertOnShift",
+) ?? true;
+
+export const patches: Patch[] = [
+  {
+    find: ',"Message")}function',
+    replace: {
+      // FIXME: terrible find. simply terrible. what the hell
+      match: /(.{1,}),shouldMention:!(.{1,2})\.shiftKey/,
+      replacement: (_, prevVal, keyboard) =>
+        `${prevVal},shouldMention:require("customReplyMention_utils").shouldPing(${keyboard}.shiftKey)`,
+    },
+  },
+];
+
+export const webpackModules: Record<string, ExtensionWebpackModule> = {
+  utils: {
+    entrypoint: true,
+    run: (module, _, __) => {
+      module.exports = {
+        shouldPing: function (isShiftPressed: boolean) {
+          console.log(
+            "whar",
+            isShiftPressed,
+            defaultPing,
+            shouldInvertOnShift,
+            shouldInvertOnShift && isShiftPressed ? !defaultPing : defaultPing,
+          );
+
+          return shouldInvertOnShift && isShiftPressed
+            ? !defaultPing
+            : defaultPing;
+        },
+      };
+    },
+  },
+};
